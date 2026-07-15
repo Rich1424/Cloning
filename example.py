a8c865923e2e68ab2c0536e63c0dc14da57be1eb
clone <clone@example.com>
2026-07-15 00:49:21 +0000

    I created the example.py file and merged?

--- example.py
+++ example.py
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 
+=======
+def git_opeation():
+ print("I am adding example.py file to the remote repository.")
+git_opeation()
+>>>>>>> 496b80c (I added a new py file called example.py)
